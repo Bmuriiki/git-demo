e8a606b0a5579a1e43c52ac622da86874421ff1b
Bmuriiki <muriikibrian@gmail.com>
2026-06-11 20:06:49 +0300

    Added department query in practice_1.sql

---
+++ Other/Data Engineering/practice_1.sql
@@ -0,0 +1,34 @@
+SELECT *
+FROM departments
+
+SELECT employee_id as id
+FROM employees
+
+SELECT *
+FROM regions
+
+SELECT *
+FROM employees
+
+SELECT 
+first_name,
+last_name,
+email
+FROM employees
+WHERE region_id= 3
+
+SELECT 
+	DISTINCT(department) as "Unique Departments"
+FROM employees
+
+SELECT 
+	gender,
+	COUNT(gender) as "No. of Employees"
+FROM employees
+WHERE salary>10000
+GROUP BY gende
+
+
+
+
+
